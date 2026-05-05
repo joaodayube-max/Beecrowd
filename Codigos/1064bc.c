cf2c47de1259cb1104fd470ba0436d46b0654fce
JoaoPedroDayube <joaodayube@gmail.com>
2026-05-04 21:15:16 -0300

    Adicionei 1064 Beecrowd

--- Codigos/1064bc.c
+++ Codigos/1064bc.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
 int main (){
-    double valor;
+    double valor, media, soma = 0;
     int positivos = 0;
 
     for (int i = 1; i <= 6; i++){
         scanf("%lf", &valor);
         if (valor > 0){
             positivos++;
+            soma += valor;
         }
     }
     printf("%d valores positivos\n", positivos);
+
+    if (positivos > 0){
+        media = soma / positivos;
+        printf("%.1lf\n", media);
+    }
 
     return 0;
 }
